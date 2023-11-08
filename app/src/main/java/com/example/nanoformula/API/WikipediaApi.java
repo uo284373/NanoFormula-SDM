5d1453e78d054c4669e293cd612cb49248fd29af
carlos <uo284373@uniovi.es>
2023-11-08 19:00:36 +0100

    08/11/2023 - Carlos - Solucionado error de resolucion

--- app/src/main/java/com/example/nanoformula/API/WikipediaApi.java
+++ app/src/main/java/com/example/nanoformula/API/WikipediaApi.java
@@ -9,7 +9,7 @@ import retrofit2.http.Query;
 
 public interface WikipediaApi {
 //http://en.wikipedia.org/w/api.php?action=query&titles=Lando_Norris&prop=pageimages&format=json&formatversion=2
-    @GET("w/api.php?action=query&prop=pageimages&format=json&formatversion=2")
+    @GET("w/api.php?action=query&prop=pageimages&format=json&formatversion=2&pithumbsize=500")
     Call<DriverImage> getImageDriver(
         @Query("titles") String driverName
     );
